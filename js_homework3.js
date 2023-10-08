fd8e8dc401b1a7ae1514b9c4e59cc5bec311c443
Eldmitry <dp290691bdo@gmail.com>
2023-10-08 20:15:18 +0300

    update 2

--- js_homework3.js
+++ js_homework3.js
@@ -2,6 +2,9 @@ let age = prompt('Вкажіть ваш вік');
 if (age % 10 === 0 || age >= 5 && age <= 20 || age === 0){
     console.log(age + 'років')
 }
+else if (age % 100 === 11) {
+    console.log(age + 'років')
+}
 else if (age % 10 === 1) {
     console.log(age + 'рік')
 }
