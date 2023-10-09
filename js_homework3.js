e6ff8517656433d308e9a58fc53a1a5bb82d446c
Dmytro Bilozertsev <74272379+ElDmitry@users.noreply.github.com>
2023-10-09 11:27:40 +0300

    Update 2 js_homework3.js

--- js_homework3.js
+++ js_homework3.js
@@ -1,5 +1,7 @@
 let age = prompt('Вкажіть ваш вік');
-if (age % 10 === 0 || age >= 5 && age <= 20 || age === 0){
+if (age < 0)
+    console.log('ERROR')
+else if (age % 10 === 0 || age >= 5 && age <= 20 || age === 0){
     console.log(age + 'років')
 }
 else if (age % 100 === 11) {
@@ -14,8 +16,7 @@ else if (age % 10 >= 2 && age % 10 <= 4) {
 else if (age % 10 >= 5 && age % 10 <= 9){
     console.log(age + 'років')
 }
-else if (age < 0)
-    console.log('ERROR')
+
 
 
 
