5009ec06593283eb947f2752dcf033578ebb9746
Dmytro Bilozertsev <74272379+ElDmitry@users.noreply.github.com>
2023-10-10 13:51:09 +0300

    Update with validation js_homework3.js

--- js_homework3.js
+++ js_homework3.js
@@ -1,7 +1,14 @@
 let age = prompt('Вкажіть ваш вік');
-if (age < 0)
-    console.log('ERROR')
-else if (age % 10 === 0 || age >= 5 && age <= 20 || age === 0){
+if (age < 0){
+    console.log('Число не має бути відємним')
+}
+else if (age === '0'){
+    console.log(age + 'років')
+}
+else if (!+age){
+    console.log('Введіть число')
+}
+else if (age % 10 === 0 || age >= 5 && age <= 20){
     console.log(age + 'років')
 }
 else if (age % 100 === 11) {
@@ -10,13 +17,12 @@ else if (age % 100 === 11) {
 else if (age % 10 === 1) {
     console.log(age + 'рік')
 }
-else if (age % 10 >= 2 && age % 10 <= 4) {
+else if (age % 10 >= 2 && age % 10 <= 4){
     console.log(age + 'роки')
 }
 else if (age % 10 >= 5 && age % 10 <= 9){
     console.log(age + 'років')
 }
-
 
 
 
