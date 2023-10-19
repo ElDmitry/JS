72db500ae0da98dda7f75b7e249dc5cb6ca78f8f
Eldmitry <dp290691bdo@gmail.com>
2023-10-19 16:36:43 +0300

    update JS_3

--- JS_3.js
+++ JS_3.js
@@ -1,12 +1,36 @@
 function IsNaN(value) {
-        if (typeof value === 'number' && value === value) {
+    let a;
+    if (typeof value === 'string') {
+        a = value.trim();
+    }
+    else {
+        a = value;
+    }
+    if (typeof a === 'string' && !+a && !a.includes(',')){
         return false;
     }
-   else {
-       return true;
+    if ((+value && value === value) || value === null) {
+        return false;
+    }
+    else {
+        return true;
     }
 }
-console.log(IsNaN(7));
+
+
+console.log(IsNaN(NaN));        // true
+console.log(IsNaN(undefined));  // true
+console.log(IsNaN({}));         // true
+//
+console.log(IsNaN(true));       // false
+console.log(IsNaN(null));       // false
+console.log(IsNaN(37));         // false
+//
+console.log(IsNaN("37"));       // false
+console.log(IsNaN("37.37"));    // false
+console.log(IsNaN(""));         // false
+console.log(IsNaN(" "));        // false
+console.log(IsNaN("37,5"));     // true
 
 
 
