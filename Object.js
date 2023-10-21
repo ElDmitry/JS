832f7d9e0ccab977ec8decf45ce3584d1a1f1e14
Eldmitry <dp290691bdo@gmail.com>
2023-10-21 22:48:17 +0300

    add homework

---
+++ Object.js
@@ -0,0 +1,15 @@
+let dog = {
+    name: 'Lucky',
+    breed: 'Labrador',
+    age: 3,
+    getInfo: function() {
+        console.log(dog)
+    }
+}
+
+
+dog.getInfo();
+dog.vaccine = true;
+dog.getInfo();
+dog.color = 'Golden';
+dog.getInfo();
