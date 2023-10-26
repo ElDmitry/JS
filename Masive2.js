263117fba3aa142caa17d10922800c165706ebae
Eldmitry <dp290691bdo@gmail.com>
2023-10-26 15:06:00 +0300

    add homework

---
+++ Masive2.js
@@ -0,0 +1,60 @@
+const company = {
+    name: 'Велика Компанія',
+    type: 'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків'
+        }
+    ]
+}
+
+const findValueByKey = (companyObj, companyName) => {
+    const values = [...companyObj.clients];
+
+    while (values.length !== 0) {
+        const current = values.shift();
+        if (current.name === companyName) {
+            return current;
+        }
+        if (current.partners && current.partners.length !== 0) {
+            values.push(...current.partners);
+        }
+    }
+
+    return null;
+}
+console.log(findValueByKey(company, 'Клієнт 1.2.3'))
+
